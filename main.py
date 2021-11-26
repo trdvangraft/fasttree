3cb9b946ff90caf0abf0c49738b7c5974c18c495
trdvangraft <tijmengraft@gmail.com>
2021-11-26 10:21:02 +0100

    setup

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import numpy as np
-import pandas as pd
+from itertools import product
 
-from Bio import AlignIO
+
+def count_v2(motifs, k):
+    count = {nucleotide: [1]*k for nucleotide in "ACTG"}
+    for i, j in product(range(len(motifs)), range(k)):
+        count[motifs[i][j]][j] += 1
+    return count
 
 
 def read_atl(file_name):
-    format = "fasta"
-    return AlignIO.read(file_name, format)
+    with open(file_name) as f:
+        lines = f.readlines()
+        return {lines[i].strip(">").strip("\n"): lines[i+1].strip("\n") for i in range(0, len(lines), 2)}
 
 
 if __name__ == "__main__":
-    print(read_atl("data/test-small.aln"))
+    print(read_atl("data/fasttree-input.aln"))
