e653ccd3066fd8d8b80c8ccdfd916f014e521a16
tijmen van graft <tijmengraft@gmail.com>
2021-12-05 19:11:01 +0100

    profile distance

--- src/profile.py
+++ src/profile.py
@@ -6,6 +6,7 @@ class Profile:
     def __init__(self, dna) -> None:
         self.motifs = dna if type(dna) == list else [dna]
         self.__count_profile = self.__calculate_count_profile()
+        self.__weight_profile = [1] * len(dna[0])
     
     def get_frequency_profile(self) -> dict:
         return {symbol: [count / len(self.motifs) for count in self.__count_profile[symbol]] for symbol in self.__count_profile}
@@ -16,6 +17,15 @@ class Profile:
     def combine(self, profile: Profile) -> Profile:
         new_motifs = self.motifs + profile.motifs
         return Profile(new_motifs)
+    
+    def distance(self, profile: Profile):
+        number_of_positions = len(self.dna[0])
+        denom = sum([self.__weight_profile[i] * profile.__weight_profile[i] for i in range(number_of_positions)])
+        top = sum([(1 - sum([a * b for a, b in zip(self.get_freq(i), profile.get_freq(i))])) * (self.__weight_profile[i] * profile.__weight_profile[i]) for i in range(number_of_positions)])        
+        return (denom if denom > 0 else 0.01, top/denom if denom > 0 else 1)
+            
+    def get_freq(self, i):
+        return [self.__count_profile[key][i] for key in self.__count_profile.keys()]
 
     def __calculate_count_profile(self) -> dict:
         k = len(self.motifs[0])
